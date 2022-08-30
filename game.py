276cf764cf59fb8a8411b8c4b468809643ba650d
Maxens Janin <75015357+skycoore@users.noreply.github.com>
2022-08-30 18:44:37 +0200

    Update game.py

--- game.py
+++ game.py
@@ -16,57 +16,46 @@ class Game:
             "void": "."
         }
 
-        self.refresh() # execute the refresh module
-        self.loop() # execute the loop module
+        self.refresh()
+        self.loop()
 
     def loop(self):
         while True:
-            # general
-            if keyboard.is_pressed("esc"): # if user press escape key, 
-                exit() # stop the process
-            
-            # player movements
-            for y in range(len(self.map)): # get player y in self.map list
-                try:
-                    x = self.map[y].index("player") # get player x with y in self.map
-                    
-                    if keyboard.is_pressed("z"): # if user press s,
-                        if self.map[y-1][x] == "void": # if there isn't collision,
-                            self.map[y][x] = "void" # set the current player position to "void",
-                            self.map[y-1][x] = "player" # set the new player position
-                            self.refresh() # to refresh the window
+            if keyboard.is_pressed("esc"):
+                exit()
 
-                    if keyboard.is_pressed("s"): # if user press z,
-                        if self.map[y+1][x] == "void": # if there isn't collision,
-                            self.map[y][x] = "void" # set the current player position to "void",
-                            self.map[y+1][x] = "player" # set the new player position
-                            self.refresh() # to refresh the window
+            if keyboard.is_pressed("z"):
+                self.move("player", 0, -1)
 
-                    if keyboard.is_pressed("q"): # if user press q,
-                        if self.map[y][x-1] == "void": # if there isn't collision,
-                            self.map[y][x] = "void" # set the current player position to "void",
-                            self.map[y][x-1] = "player" # set the new player position
-                            self.refresh() # to refresh the window
+            if keyboard.is_pressed("s"):
+                self.move("player", 0, +1)
 
-                    if keyboard.is_pressed("d"): # if user press d,
-                        if self.map[y][x+1] == "void": # if there isn't collision,
-                            self.map[y][x] = "void" # set the current player position to "void",
-                            self.map[y][x+1] = "player" # set the new player position
-                            self.refresh() # to refresh the window
+            if keyboard.is_pressed("q"):
+                self.move("player", -1, 0)
 
-                except: pass # if player isn't have the current y ("player_y")
+            if keyboard.is_pressed("d"):
+                self.move("player", +1, 0)
 
-    def refresh(self): # define refresh module and set Y to 0
-        os.system("clear" if os.name != "nt" else "cls") # clear the current window
-        print("linktr.ee/skycore") # ad :)
-        for y in range(len(self.map)): # get y in self.map list
-            line = ""
-            for x in range(len(self.map[y])): # get x from y in self.map list
-                type = self.map[y][x] # get the type of the current zone
-                line += self.textures[type] # add the texture to the line
+    def move(self, target, add_to_x, add_to_y):
+        for y in range(len(self.map)):
+            try:
+                x = self.map[y].index(target)
 
-                if len(line) == len(self.map[y]): # if "y" isn't as last "y" named "Y" 
-                    print(line) # show the line
-                    line = "" # reset the line
+                if self.map[y+(add_to_y)][x+(add_to_x)] == "void":
+                    self.map[y][x] = "void"
+                    self.map[y+(add_to_y)][x+(add_to_x)] = target
+                    self.refresh()
+            except: pass
 
-Game() # execute Game
+    def refresh(self):
+        os.system("clear" if os.name != "nt" else "cls")
+        print("linktr.ee/skycore")
+        for y in range(len(self.map)):
+            for x in range(len(self.map[y])):
+                type = self.map[y][x]
+                print(self.textures[type], end="")
+
+                if x == len(self.map):
+                    print("")
+
+Game()
